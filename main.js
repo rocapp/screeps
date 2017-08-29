ded5f5cbd558d90ff07a2fdad9a8dac3a63dbc4e
william barnett <whbdupree@gmail.com>
2017-08-29 19:54:19 -0400

    creeps are too big

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ module.exports.loop = function () {
 	    roleRemember.make(Game);
 	    */
 	    var sources = Game.spawns['s1'].room.find(FIND_SOURCES)
-	    Game.spawns['s1'].createCreep( [WORK, CARRY, CARRY, MOVE, MOVE, WORK, CARRY, MOVE], roles[r] + Game.time.toString(),
+	    Game.spawns['s1'].createCreep( [WORK, CARRY, CARRY, MOVE, MOVE, WORK], roles[r] + Game.time.toString(),
 					{ role: roles[r],
 					  working: false,
 					  target: sources[0].id } );     
@@ -39,7 +39,7 @@ module.exports.loop = function () {
 	    break;
 	}
 	else if (b < myCreeps[roles[r]]) {
-            Game.spawns['s1'].createCreep( [WORK, CARRY, CARRY, MOVE, MOVE, WORK, CARRY, MOVE], roles[r] + Game.time.toString(), { role: roles[r] } );     
+            Game.spawns['s1'].createCreep( [WORK, CARRY, CARRY, MOVE, MOVE, WORK], roles[r] + Game.time.toString(), { role: roles[r] } );     
             break;
         }    
         
