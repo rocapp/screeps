7cc767b4fd42e5201511fdfc60a736ac2380e7a8
william barnett <whbdupree@gmail.com>
2017-08-28 20:23:10 -0400

    test target saving on upgrader

--- role.upgrader.js
+++ role.upgrader.js
@@ -3,22 +3,24 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function(creep) {
 	    if(creep.memory.upgrading && creep.carry.energy == 0) {
-            creep.memory.upgrading = false;
-            creep.say('i harvest');
+		creep.memory.upgrading = false;
+		creep.say('i harvest');
+		var sources = creep.room.find(FIND_SOURCES);
+		creep.memory.target = sources[0].id
 	    }
 	    if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.upgrading = true;
 	        creep.say('i upgrade');
+		creep.memory.target = creep.room.controller.id
 	    }
         if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
+            if(creep.upgradeController(creep.memory.target) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.memory.target);
             }
         }
 	    else {
-	        var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
+		if(creep.memory.target == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.memory.target, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
 
