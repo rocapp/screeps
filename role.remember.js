04373ddcd711ba71d145a70f714f7aa9a504c5c3
william barnett <whbdupree@gmail.com>
2017-08-28 22:02:58 -0400

    memory statement was in the wrong if

--- role.remember.js
+++ role.remember.js
@@ -4,13 +4,13 @@ var roleRemember = {
     run: function(creep) {
 	if(creep.memory.working && creep.carry.energy == 0) {
 	    creep.memory.working = false;
-	    creep.memory.target = creep.room.controller.id;
+
 	    creep.say('i harvest');
 	}
 	if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
 	    creep.memory.working = true;
 	    creep.say('i upgrade');
-
+	    creep.memory.target = creep.room.controller.id;
 	}
         if(creep.memory.working) {
 	    
