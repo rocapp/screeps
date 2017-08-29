d98485fab94c24fa2b1db425d4f3986f5d4fe997
william barnett <whbdupree@gmail.com>
2017-08-28 20:58:13 -0400

    rolled upgrader back to initial commit

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,39 +1,28 @@
 var roleUpgrader = {
-
+    
     /** @param {Creep} creep **/
     run: function(creep) {
-	if(creep.memory.upgrading && creep.carry.energy == 0) {
-	    creep.memory.upgrading = false;
-	    creep.say('i harvest');
-
-	}
-	if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-	    creep.memory.upgrading = true;
-	    creep.say('i upgrade');
-
-	}
+ 	if(creep.memory.upgrading && creep.carry.energy == 0) {
+            creep.memory.upgrading = false;
+            creep.say('i harvest');
+ 	}
+ 	if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+ 	    creep.memory.upgrading = true;
+ 	    creep.say('i upgrade');
+ 	}
         if(creep.memory.upgrading) {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
             }
         }
-	else {
-	    var sources = creep.room.find(FIND_SOURCES);
-	    if(creep.transfer(sources[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+ 	else {
+ 	    var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
-
+	
     }
-
-    make: function(spawn) {
-	var sources = creep.room.find(FIND_SOURCES)
-	Game.spawns[spawn].createCreep( [WORK, CARRY, CARRY, MOVE, MOVE, WORK], roles[r] + Game.time.toString(),
-					{ role: roles[r],
-					  upgrading: false,
-					  target: sources[0].id } );     
-    }
-
 };
 
-module.exports = roleUpgrader;
+module.exports = roleUpgrader; 
