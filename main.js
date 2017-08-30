dcda62261b21eecba88827c055d46447531f398b
william barnett <whbdupree@gmail.com>
2017-08-29 20:47:52 -0400

    ref to roomname in a string

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function defendRoom(room) {
     var hostiles = room.find(FIND_HOSTILE_CREEPS);
     if(hostiles.length > 0) {
         var username = hostiles[0].owner.username;
-        Game.notify(`User ${username} spotted in room ${roomName}`);
+        Game.notify(`User ${username} spotted in room`);
         var towers = room.find(
             FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
         towers.forEach(tower => tower.attack(hostiles[0]));
